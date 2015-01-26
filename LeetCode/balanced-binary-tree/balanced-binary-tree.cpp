880d7de4d10891fa4d39d1217f8992f73bbd3c31
luikimfai <luikimfai@gmail.com>
2015-01-26 20:15:22 +0800

    majority-element

--- LeetCode/balanced-binary-tree/balanced-binary-tree.cpp
+++ LeetCode/balanced-binary-tree/balanced-binary-tree.cpp
@@ -3,80 +3,34 @@
 
 #include "stdafx.h"
 
+/*
+Given a binary tree,
+determine if it is height-balanced.
 
+For this problem, 
+a height-balanced binary tree is defined as a binary tree 
+in which the depth of the two subtrees of 
+every node never differ by more than 1.
 
+*/
 
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 public:
     bool isBalanced(TreeNode *root) 
 	{
-		if (!root)
-		{
-			return true;
-		}
-		int left = maxDepth(root->left);
-		int right = maxDepth(root->right);
-        if (left - right > 1 || right - left> 1 )
-        {
-			return false;
-        }
-
-		if (!isBalanced(root->right))
-		{
-			return false;
-		}
-		if (!isBalanced(root->left))
-		{
-			return false;
-		}
-
-		return true;
+        
     }
-
-	int maxDepth(TreeNode* root)
-	{
-		if (root == NULL)
-		{
-			return 0;
-		}
-
-		int left = maxDepth(root->left);
-		int right = maxDepth(root->right);
-
-		if (left > right)
-		{
-			 return 1 + left;
-
-		}
-		else
-		{
-			return 1+ right;
-		}
-	}
-
 };
-
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	 TreeNode* a = new TreeNode(1);
-	 TreeNode* b = new TreeNode(2);
-	 TreeNode* c = new TreeNode(3);
-
-	a->right = b;
-	b->right = c;
-
-
-	Solution so;
-	so.isBalanced(a);
-
 	return 0;
 }
 
---
+++ LeetCode/majority-element/majority-element.cpp
@@ -0,0 +1,77 @@
+﻿// majority-element.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <map>
+
+using namespace std;
+/*
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+*/
+
+class Solution {
+public:
+	int majorityElement(vector<int> &num) 
+	{
+		map<int,int> amap;
+		int nSize = num.size();
+		for (int i = 0; i < nSize; i++)
+		{
+			int val = num[i];
+			if (amap.find(val) == amap.end())
+			{
+				amap[val] = 1;
+				continue;
+			}
+
+			amap[val]++;
+		}
+
+		map<int,int>::iterator it;
+		for (it = amap.begin();it != amap.end(); it++)
+		{
+			if (it->second >(nSize/2) )
+			{
+				return it->first;
+			}
+		}
+
+		return 0;
+	}
+};
+
+
+// Moore Voting Algorithm
+// Refer to: 
+// http://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html
+int majorityElement(vector<int> &num) {
+	int majority;
+	int cnt = 0;
+	for(int i=0; i<num.size(); i++){
+		if ( cnt ==0 ){
+			majority = num[i];
+			cnt++;
+		}else{
+			majority == num[i] ? cnt++ : cnt --;
+			if (cnt >= num.size()/2+1) return majority;
+		}
+	}
+	return majority;
+
+}
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Solution so;
+	int a[] = {1,1,1,2,2,3,1,3,3};
+	vector<int> num(a,a + sizeof(a)/sizeof(a[0]));
+	so.majorityElement(num);
+	majorityElement(num);
+	return 0;
+}
+
