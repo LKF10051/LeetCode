5751d5b8304d93eee276b1df37c388dbae96ae45
luikimfai <luikimfai@gmail.com>
2016-02-18 00:43:26 -0800

    test

--- linux/001_two_sum/two_sum.cpp
+++ linux/001_two_sum/two_sum.cpp
@@ -1,9 +1,38 @@
-#include<string>
+#include <string>
+#include <vector>
+#include <unordered_map>
 using namespace std;
+
+
+vector<int> twoSum1(vector<int>& numbers, int target) 
+{
+    unordered_map<int, int> m;
+    vector<int> result;
+    for(size_t i=0; i<numbers.size(); i++)
+    {
+	// not found the second one
+	if (m.find(numbers[i])==m.end() ) 
+	{ 
+	    // store the first one poisition into the second one's key
+	    m[target - numbers[i]] = i; 
+	}
+	else 
+	{ 
+	    // found the second one
+	    result.push_back(m[numbers[i]]);
+	    result.push_back(i);
+	    break;
+	}
+    }
+
+    return result;
+}
+
+
 int main()
 { 
-   string aa;
-   aa.push_back(aa);
-
-   return 0;
+    vector<int> nums = {1,2,3,4,5,6};
+    vector<int> ret = twoSum1(nums, 6);
+    
+    return 0;
 }
